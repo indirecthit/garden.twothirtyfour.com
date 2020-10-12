1a35c579666c882a03a627b4daa183690eaab2f2
Chris Long <clong@twothirtyfour.com>
2020-10-12 11:13:17 -0400

    Require now only handles image files (no requiring unsupported files)

--- components/obsidian-image.js
+++ components/obsidian-image.js
@@ -1,9 +1,12 @@
 const isImgExt = (value) => /\.(svg|png|jpg|jpeg|gif)/.test(value)
 
+// See https://webpack.js.org/guides/dependency-management/#require-with-expression
+// This helps Webpack understand what should be imported at compile time
+const requireContext = require.context('../data', true, /\.(svg|png|jpg|jpeg|gif)$/)
+
 function ObsidianImage({ node }) {
-  console.log("XXXX")
   if(isImgExt(node.url)) {
-    const multipleSizes = require(`../data/${node.url}?resize&sizes[]=300&sizes[]=600&sizes[]=1000`);
+    const multipleSizes = requireContext(`../data/${node.url}?resize&sizes[]=300&sizes[]=600&sizes[]=1000`);
     return (
       <div>
         <img srcSet={multipleSizes.srcSet} src={multipleSizes.src} />
